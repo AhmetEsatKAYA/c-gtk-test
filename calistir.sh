3684f29234b27f0632bc618f0bca9a4e97d39bcd
AhmetEsatKAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2024-01-21 11:35:45 +0300

    Update calistir.sh
    add yes/no question

--- calistir.sh
+++ calistir.sh
@@ -1,2 +1,26 @@
+if [ -e gtkexample ]
+then
+    rm gtkexample
+    echo "derlenmis dosya silindi"
+else
+    echo "derlenmis dosya bulunamadi."
+fi
+
+echo "islem devam ediyor..."
+echo "derleme baslatiliyor..."
+
 gcc main.c -o gtkexample `pkg-config --cflags --libs gtk+-2.0`
-./gtkexample
+
+if [ -e gtkexample ]
+then
+    echo "basariyla derlendi"
+    read -p "Calistirilsin mi E/h:" choice
+    case $choice in
+    [eE]* ) ./gtkexample ;;
+    [hH]* ) echo "vazgecildi" ;;
+    *) exit ;;
+esac
+
+else
+    echo "derleme hatasi olustu."
+fi
